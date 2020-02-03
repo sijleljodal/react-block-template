0f7dd1a8d647c7c51d136baefbb4f449cc85e347
Silje Ljødal <silje.ljodal@gmail.com>
2020-02-03 09:57:05 +0100

    Updates

---
+++ precache-manifest.7167399aab538e3cb2a2d59f63c1a6e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efe3a2d13fe8ae6c794c133faac83e5b",
+    "url": "/react-block-template/index.html"
+  },
+  {
+    "revision": "1f14c675be420f604e9f",
+    "url": "/react-block-template/static/css/main.826617f2.chunk.css"
+  },
+  {
+    "revision": "06ee228ac28864e77c4e",
+    "url": "/react-block-template/static/js/2.5b959f76.chunk.js"
+  },
+  {
+    "revision": "b03343d1dfdf0242e0f4ba3806a19844",
+    "url": "/react-block-template/static/js/2.5b959f76.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1f14c675be420f604e9f",
+    "url": "/react-block-template/static/js/main.f3e2e133.chunk.js"
+  },
+  {
+    "revision": "41989dd9611a4659c79e",
+    "url": "/react-block-template/static/js/runtime-main.fb661ec1.js"
+  },
+  {
+    "revision": "3bf176e01196c650dc30f90db6295a45",
+    "url": "/react-block-template/static/media/undskr.3bf176e0.png"
+  }
+]);
